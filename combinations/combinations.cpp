942ddebafefbab506f3cc612ee705867198134e1
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-06-08 18:32:25 +0530

    Time: 29 ms (64.53%) | Memory: 9.9 MB (56.39%) - LeetSync

--- combinations/combinations.cpp
+++ combinations/combinations.cpp
@@ -2,16 +2,14 @@ class Solution {
     vector<vector<int>>ans;
 public:
     void helper(int num,int n,int k,vector<int>&temp){
-        if(num==n+1){
-            if(k==0) ans.push_back(temp);
-            return;
+        if(k==0){
+            ans.push_back(temp); return;
         }
-        if(k>0){
-            temp.push_back(num);
-            helper(num+1,n,k-1,temp);
+        for(int idx=num;idx<=n;idx++){
+            temp.push_back(idx);
+            helper(idx+1,n,k-1,temp);
             temp.pop_back();
         }
-        helper(num+1,n,k,temp);
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int>temp;
