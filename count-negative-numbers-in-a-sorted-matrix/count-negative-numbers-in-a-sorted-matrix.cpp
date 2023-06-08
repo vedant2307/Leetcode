ddaab9491a10560472c2bf80d9040588ac3e41ff
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-06-08 12:17:57 +0530

    Time: 15 ms (82.04%) | Memory: 10.5 MB (31.99%) - LeetSync

--- count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int helper(int low,int high,vector<int>&temp){
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(temp[mid]<0) high=mid-1;
-            else low=mid+1;
-        }
-        return low;
-    }
+    //Apporach 1: Brute Force = O(n*m)
+    //Approach 2 : Binary Serach = O(n*log(m)) -> Apply upper bound on reverse of array
+    //Approach 3: Two Pointer = O(n+m)
     int countNegatives(vector<vector<int>>& grid) {
-        int ans=0,n=grid.size(),m=grid[0].size();
-        for(int i=0;i<n;i++){
-            int idx=helper(0,m-1,grid[i]);
-            ans+=(m-idx);
+        int ans=0;
+        int n=grid.size(),m=grid[0].size();
+        int row=n-1,col=0;
+        while(row>=0 && col<m){
+            if(grid[row][col]<0){
+                ans+=(m-col); row--;
+            }
+            else col++;
         }
         return ans;
     }
