4a62b9477990bc20655d3b4c20b69bc11104ac58
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-06-10 00:04:10 +0530

    Time: 128 ms (57.27%) | Memory: 35.3 MB (85.71%) - LeetSync

--- rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    #define pp pair<int,int>
     vector<int> arrayRankTransform(vector<int>& arr) {
-        map<int,int>mp;
-        for(auto it:arr) mp[it]++;
-        int rank=0,n=arr.size();
-        for(auto it:mp) mp[it.first]=++rank;
-        for(int i=0;i<n;i++) arr[i]=mp[arr[i]];
-        return arr;
+        priority_queue<pp,vector<pp>,greater<pp>>pq;
+        int N=arr.size();
+        for(int i=0;i<N;i++) pq.push({arr[i],i});
+        vector<int>ans(N);
+        int t=0,prev=INT_MIN;
+        while(!pq.empty()){
+            int val=pq.top().first,idx=pq.top().second; 
+            pq.pop();
+            if(val>prev){
+                t++; prev=val;
+            }
+            ans[idx]=t;
+        }
+        return ans;
     }
 };
