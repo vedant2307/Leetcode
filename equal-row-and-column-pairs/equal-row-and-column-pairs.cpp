e68b4d358c0a642c5766bacc5ee31d4ec5b37627
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-06-13 11:07:21 +0530

    Time: 102 ms (79.79%) | Memory: 24.8 MB (82.06%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+    void transpose(vector<vector<int>>& grid,int n){
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(grid[i][j],grid[j][i]);
+            }
+        }
+        return ;
+    }
     int equalPairs(vector<vector<int>>& grid) {
         int n=grid.size(),ans=0;
         map<vector<int>,int>mp;
         for(int i=0;i<n;i++){
             mp[grid[i]]++;
         }
-        for(int j=0;j<n;j++){
-            vector<int>temp;
-            for(int i=0;i<n;i++){
-                temp.push_back(grid[i][j]);
-            }
-            if(mp.find(temp)!=mp.end()) ans+=(mp[temp]);
+        transpose(grid,n);
+        for(int i=0;i<n;i++){
+            if(mp.count(grid[i])) ans+=mp[grid[i]];
         }
         return ans;
     }
