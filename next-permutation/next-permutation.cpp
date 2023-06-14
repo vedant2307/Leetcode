476ad198c9299276e966d4be4a20fe20e36721fa
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-06-14 14:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (21.78%) - LeetSync

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -3,12 +3,11 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size(),idx1=n-2,idx2=n-1;
         while(idx1>=0 && nums[idx1]>=nums[idx1+1]) idx1--;
-        if(idx1<0) reverse(nums.begin(),nums.end());
-        else{
-            while(idx2>0 && nums[idx2]<=nums[idx1]) idx2--;
+        if(idx1>=0){
+            while(idx2>=idx1 && nums[idx2]<=nums[idx1]) idx2--;
             swap(nums[idx2],nums[idx1]);
-            reverse(nums.begin()+idx1+1,nums.end());
         }
+        reverse(nums.begin()+idx1+1,nums.end());
         return;
     }
 };
