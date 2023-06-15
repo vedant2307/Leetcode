c6b5c23ace54af07c10210356da5c39158986cac
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-06-15 20:10:20 +0530

    Time: 14 ms (43.70%) | Memory: 13.9 MB (5.05%) - LeetSync

---
+++ sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<int,int>mp;
+        for(auto it:s) mp[it]++;
+        int n=s.length();
+        vector<vector<int>>temp(n+1);
+        for(auto it:mp){
+            temp[it.second].push_back(it.first-'0');
+        }
+        string ans;
+        for(int i=n;i>=1;i--){
+            if(temp[i].size()>0){
+                for(auto it:temp[i]){
+                    int cnt=i;
+                    while(cnt--) ans+=(it+'0');
+                }
+            }
+        }
+        return ans;
+    }
+};
