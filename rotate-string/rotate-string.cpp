3d6910b648a47bcd9d8d52fc737609bcb262f5d5
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-06-15 18:23:47 +0530

    Time: 2 ms (45.54%) | Memory: 6.9 MB (17.29%) - LeetSync

--- rotate-string/rotate-string.cpp
+++ rotate-string/rotate-string.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    bool helper(string a,string b,int rot,int n){
+        for(int j=0;j<n;j++){
+            if(a[j]!=b[(rot+j)%n]) return false;
+        }
+        return true;
+    }
     bool rotateString(string s, string goal) {
         int len=s.length(),n=goal.length();
         if(len!=n) return false;
-        s=s+s;
         for(int i=0;i<n;i++){
-            string temp=s.substr(i,len);
-            if(temp==goal) return true;
+            if(helper(s,goal,i,n)) return true;
         }
         return false;
     }
