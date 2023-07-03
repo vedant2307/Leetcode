d1ae3cb79540b6bbe9cac941270e26fb5b34963c
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-07-03 15:28:40 +0530

    Time: 68 ms (97.22%) | Memory: 8.6 MB (92.59%) - LeetSync

--- maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
+++ maximum-number-of-achievable-transfer-requests/maximum-number-of-achievable-transfer-requests.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int helper(int n,vector<vector<int>>&nums,int idx,vector<int>&temp){
-        if(idx==nums.size()){
+    int helper(int m,vector<vector<int>>&nums,int idx,vector<int>&temp){
+        if(idx==m){
             for(auto it:temp){
-                if(it!=0) return INT_MIN;
+                if(it!=0) return -20;
             }
             return 0;
         }
-        temp[nums[idx][0]]--; temp[nums[idx][1]]++;
-        int pick=helper(n,nums,idx+1,temp)+1;
-        temp[nums[idx][0]]++; temp[nums[idx][1]]--;
-        int npick=helper(n,nums,idx+1,temp);
+        int from=nums[idx][0],to=nums[idx][1];
+        temp[from]--; temp[to]++;
+        int pick=helper(m,nums,idx+1,temp)+1;
+        temp[from]++; temp[to]--;
+        int npick=helper(m,nums,idx+1,temp);
         return max(pick,npick);
     }
     int maximumRequests(int n, vector<vector<int>>& requests) {
         vector<int>temp(n);
-        return helper(n,requests,0,temp);
+        int m=requests.size();
+        return helper(m,requests,0,temp);
     }
 };
