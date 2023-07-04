4f6c5918542865865b04bb2e311674287585bae9
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-07-04 16:05:36 +0530

    Time: 362 ms (86.64%) | Memory: 10.4 MB (36.99%) - LeetSync

--- burst-balloons/burst-balloons.cpp
+++ burst-balloons/burst-balloons.cpp
@@ -10,11 +10,22 @@ public:
         }
         return dp[i][j]=ans;
     }
+    // return helper(nums,1,m-2,dp);
     int maxCoins(vector<int>& nums) {
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         int m=nums.size();
-        vector<vector<int>>dp(m,vector<int>(m,-1));
-        return helper(nums,1,m-2,dp);
+        vector<vector<int>>dp(m,vector<int>(m,0));
+        for(int i=m-2;i>=1;i--){
+            for(int j=i;j<m-1;j++){
+                int ans=0;
+                for(int k=i;k<=j;k++){
+                    int temp = (nums[j+1]*nums[i-1]*nums[k]) + dp[i][k-1] + dp[k+1][j];
+                    ans=max(ans,temp);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[1][m-2];
     }
 };
