ba5d85ddd684f16f5856e319059678a9b05caafd
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-07-06 12:10:16 +0530

    Time: 10 ms (96.47%) | Memory: 8.4 MB (82.69%) - LeetSync

--- partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int helper(vector<int>&arr, int k, int n, int idx, int maxi, vector<int>&dp){
+    int helper(vector<int>&arr, int k, int n, int idx, vector<int>&dp){
         if(idx==n) return 0;
         if(dp[idx]!=-1) return dp[idx];
-        int ans=0;
+        int ans=0,maxi=-1,temp=0;
         for(int i=idx;i<min(idx+k,n);i++){
-            maxi=max(maxi,arr[i]);
-            int temp = (i-idx)+1;
-            int sum = temp*maxi + helper(arr,k,n,i+1,arr[i+1],dp);
+            maxi=max(maxi,arr[i]); temp++;
+            int sum = temp*maxi + helper(arr,k,n,i+1,dp);
             ans=max(ans,sum);
         }
         return dp[idx]=ans;
     }
+    // return helper(arr,k,n,0,dp);
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n=arr.size();
-        arr.push_back(-1);
-        vector<int>dp(n+1,-1);
-        return helper(arr,k,n,0,-1,dp);
+        vector<int>dp(n+1,0);
+        for(int idx=n-1;idx>=0;idx--){
+            int ans=0,maxi=-1,temp=0;
+            for(int i=idx;i<min(idx+k,n);i++){
+                maxi=max(maxi,arr[i]); temp++;
+                int sum = temp*maxi + dp[i+1];
+                ans=max(ans,sum);
+            }
+            dp[idx]=ans; 
+        }
+        return dp[0];
     }
 };
