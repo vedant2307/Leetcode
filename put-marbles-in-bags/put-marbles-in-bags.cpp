bd4edc568aec59bf49ec4f437c1a504b22f15c62
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-07-09 12:57:42 +0530

    Time: 209 ms (66.67%) | Memory: 57.4 MB (99.66%) - LeetSync

--- put-marbles-in-bags/put-marbles-in-bags.cpp
+++ put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     #define ll long long
-    long long putMarbles(vector<int>& weights, int k) 
-    {
-        ll ans = 0LL;
-        int n=weights.size();
-        for(int i=0;i<weights.size()-1;i++)
-            weights[i] += weights[i+1];
-        weights.pop_back();
-        sort(weights.begin(),weights.end());
-        for(int i=0;i<k-1;i++)
-            ans+=weights[n-i-2]-weights[i];
+    long long putMarbles(vector<int>& w, int k) {
+        ll ans=0LL;
+        int n=w.size();
+        for(int i=0;i<n-1;i++) w[i]+=w[i+1];
+        w.pop_back();
+        sort(w.begin(),w.end());
+        for(int i=0;i<k-1;i++) ans+= (ll)(w[n-i-2]-w[i]);
         return ans;
     }
 };
