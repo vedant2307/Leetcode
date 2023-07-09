e59fafb8c0dad549a4ca11e944edceadfdb1b496
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-07-09 17:28:44 +0530

    Time: 95 ms (63.98%) | Memory: 14.2 MB (93.79%) - LeetSync

---
+++ majority-element-ii/majority-element-ii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        cnt1=0 
+        cnt2=0
+        a=-(1e9+1)
+        b=-(1e9+1)
+        n=len(nums)
+        for idx in nums:
+            if(cnt1==0 and idx!=b): 
+                cnt1=1
+                a=idx
+            elif(cnt2==0 and idx!=a): 
+                cnt2=1
+                b=idx
+            elif(idx==a): cnt1+=1
+            elif(idx==b): cnt2+=1
+            else:
+                cnt1-=1
+                cnt2-=1
+        cnt1=0
+        cnt2=0
+        for idx in nums:
+            if(idx==a): cnt1+=1
+            elif(idx==b): cnt2+=1
+        lst=[]
+        if(cnt1>n/3): lst.append(a)
+        if(cnt2>n/3): lst.append(b) 
+        return lst
