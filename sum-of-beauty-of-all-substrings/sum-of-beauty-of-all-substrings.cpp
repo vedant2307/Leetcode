9a36820ddf7e106cd22c1b6110b51963a6431ffd
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-07-10 13:28:18 +0530

    Time: 1004 ms (5.01%) | Memory: 173 MB (5.01%) - LeetSync

--- sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -4,13 +4,13 @@ public:
         int n=s.size(),ans=0;
         for(int i=0;i<n;i++){
             unordered_map<char,int>fr;
+            multiset<int>ms;
             for(int j=i;j<n;j++){
+                if(ms.find(fr[s[j]])!=ms.end())
+                    ms.erase(ms.find(fr[s[j]]));
                 fr[s[j]]++;
-                int maxi=0,mini=n+1;
-                for(auto it:fr){
-                    maxi=max(maxi,it.second); mini=min(mini,it.second);
-                }
-                ans+=(maxi-mini);
+                ms.insert(fr[s[j]]);
+                ans+=(*ms.rbegin()-*ms.begin());
             }
         }
         return ans;
