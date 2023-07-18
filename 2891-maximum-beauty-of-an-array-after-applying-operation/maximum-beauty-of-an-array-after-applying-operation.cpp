90d5bb4d84ef4e050585a5676feec8f840590797
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-07-18 15:10:14 +0530

    Time: 591 ms (49.59%) | Memory: 295.3 MB (5.41%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -4,14 +4,17 @@ public:
        int n=nums.size();
        int t=1e5;
        int s=3*t+3;
+       int mini=1e9,maxi=-1e9;
        vector<int>temp(s,0);
        for(auto it:nums){
            int a=(it-k)+t,b=(it+k)+t;
            temp[a]++;
            temp[b+1]--;
+           mini=min(mini,a);
+           maxi=max(maxi,b);
        } 
        int ans=0,sum=0;
-       for(int i=1;i<s;i++){
+       for(int i=mini;i<=maxi;i++){
            sum+=temp[i];
            ans=max(ans,sum);
        }
