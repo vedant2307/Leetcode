56283eff445d6f3b3cdf80b8e6169a5a4c1628c1
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-08-09 18:12:14 +0530

    Time: 19 ms (62.94%) | Memory: 12.9 MB (41.98%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -6,10 +6,17 @@ public:
         if(nums1[i]==nums2[j]) return dp[i][j]=1+helper(nums1,nums2,i-1,j-1,dp);
         return dp[i][j]=max(helper(nums1,nums2,i-1,j,dp),helper(nums1,nums2,i,j-1,dp));
     }
+    // return helper(nums1,nums2,n-1,m-1,dp);
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size(),m=nums2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1));
         if(nums1==nums2) return n;
-        return helper(nums1,nums2,n-1,m-1,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums1[i-1]==nums2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
