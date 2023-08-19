49ba6fdfeb794ba67100b7803c6a05a697637011
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-08-19 14:48:18 +0530

    Time: 122 ms (63.77%) | Memory: 119.9 MB (16.30%) - LeetSync

--- 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
+++ 1996-number-of-ways-to-rearrange-sticks-with-k-sticks-visible/number-of-ways-to-rearrange-sticks-with-k-sticks-visible.cpp
@@ -10,8 +10,21 @@ public:
         ll npick=(ll)(n-1)*helper(n-1,k,dp);
         return dp[n][k]=(pick+npick)%mod;
     }
+    // return (int)(helper(n,k,dp));   
     int rearrangeSticks(int n, int k) {
-        vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        return (int)(helper(n,k,dp));   
+        vector<vector<ll>>dp(n+1,vector<ll>(k+1));
+        for(int i=1;i<=2;i++){
+            for(int j=1;j<=i && j<=k;j++){
+                dp[i][j]=1;
+            }
+        }
+        for(int i=3;i<=n;i++){
+            for(int j=1;j<=i && j<=k;j++){
+                ll pick=dp[i-1][j-1];
+                ll npick=(ll)(i-1)*dp[i-1][j];
+                dp[i][j]=(pick+npick)%mod;
+            }
+        }
+        return (int)dp[n][k];
     }
 };
