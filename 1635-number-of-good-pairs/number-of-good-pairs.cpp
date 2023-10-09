f1826df38b7ab1762654f05ac94ece02b65dea06
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-10-09 18:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (24.92%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         unordered_map<int,int>mp;
-        for(auto it:nums) mp[it]++;
         int ans=0;
-        for(auto it:mp){
-            int f=it.second;
-            ans+= (f*(f-1))/2;
-        }
+        for(auto it:nums) ans+=mp[it]++;
         return ans;
     }
 };
