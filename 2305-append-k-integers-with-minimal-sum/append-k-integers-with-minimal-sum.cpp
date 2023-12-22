b58457a1950dc2ddc8f2b6e251a671cf636a6c17
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-12-22 18:26:23 +0530

    Time: 193 ms (52.97%) | Memory: 105 MB (5.02%) - LeetSync

---
+++ 2305-append-k-integers-with-minimal-sum/append-k-integers-with-minimal-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    long long helper(long long num) {
+        return (num*(num+1))/2;
+    }
+
+    long long minimalKSum(vector<int>& nums, int k) {
+        long long sum=0LL;
+        unordered_set<int>st;
+        for(auto it:nums) st.insert(it);
+        vector<int>temp;
+        for(auto it:st) temp.push_back(it); 
+        sort(temp.begin(),temp.end());
+
+        int low=0,high=temp.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int cnt=temp[mid]-(mid+1);
+            if(cnt<k) low=mid+1;
+            else high=mid-1; 
+        }
+        if(high==-1){
+            long long num=k;
+            sum=helper(num);
+        }
+        else{
+            sum=helper(temp[high]);
+            for(int i=0;i<=high;i++) sum-=temp[i];
+            int miss=temp[high]-(high+1);
+            k = k-miss;
+            long long n1=temp[high],n2=temp[high]+k;
+            sum = sum + (helper(n2)-helper(n1));
+        }
+        return sum;
+    }
+};
+
