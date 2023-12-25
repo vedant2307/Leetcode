823f37bc737746dd0f7760ec38620fd690dabf20
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2023-12-25 11:30:06 +0530

    Time: 104 ms (24.37%) | Memory: 67.1 MB (59.50%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -12,7 +12,7 @@ public:
         int n=arr.size();
         int r=n-1;
         while(r>0 && arr[r]>=arr[r-1]) r--;
-        if(r==0) return r;
+        //if(r==0) return r;
         int ans = r;
         for(int l=0;l<r && (l==0 || arr[l-1]<=arr[l]); l++){
             r = helper(arr,r,n-1,arr[l]);
