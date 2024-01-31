fa4dd9f7a9946772a17ec374e8a1ebc2b7669073
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-01-31 23:18:25 +0530

    Time: 114 ms (84.79%) | Memory: 83.2 MB (44.47%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.cpp
+++ 1814-jump-game-vi/jump-game-vi.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
         vector<int>dp(size(nums));
-        multiset<int>st({dp[0]=nums[0]});
+        deque<int>q{0};
+        dp[0]=nums[0];
 
         for(int idx=1;idx<size(nums);idx++){
-            if(idx>k) st.erase(st.find(dp[idx-k-1]));
-            st.insert({dp[idx]=*rbegin(st)+nums[idx]});
+            if(q.front()<idx-k) q.pop_front();
+            dp[idx] = dp[q.front()]+nums[idx];
+            while(!q.empty() && dp[q.back()]<=dp[idx]) q.pop_back();
+            q.push_back(idx);
         }
 
         return dp.back();
