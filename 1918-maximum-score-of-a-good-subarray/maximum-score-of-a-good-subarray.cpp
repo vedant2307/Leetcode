4e28e39fb442a7cb2a8e1309ec93d3daafb2d663
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-02-02 16:21:38 +0530

    Time: 149 ms (45.72%) | Memory: 93.5 MB (32.26%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.cpp
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.cpp
@@ -6,7 +6,7 @@ public:
         st.push(-1);
         int ans=0,n=arr.size();
         for(int idx=0;idx<n;idx++){
-            while(st.size()>1 && arr[st.top()]>arr[idx]){
+            while(st.size()>1 && arr[st.top()]>=arr[idx]){
                 int top=st.top(); st.pop();
                 int left=top-st.top(),right=idx-top;
                 if(k>st.top() && k<idx) ans=max(ans,(left+right-1)*arr[top]);
