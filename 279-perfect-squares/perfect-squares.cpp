afe9cece0ee126071ab6543218d72505559b1265
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-02-08 15:03:38 +0530

    Time: 2704 ms (5.01%) | Memory: 614.4 MB (5.32%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int helper(int n,vector<int>&dp){
-        if(n==0) return 0;
-        if(dp[n]!=-1) return dp[n];
-        int ans=1e9;
-        for(int i=sqrt(n);i>=1;i--){
-            int t=i*i;
-            if(t<=n) ans=min(ans,helper(n-t,dp)+1);
-        }
-        return dp[n]=ans;
-    }
     int numSquares(int n) {
-        vector<int>dp(n+1,-1);
-        return helper(n,dp);
+        queue<pair<int,int>>q;
+        q.push({n,0});
+        while(!q.empty()){
+            auto front=q.front(); q.pop();
+            int num=front.first, ans=front.second;
+            if(num==0) return ans;
+            for(int idx=sqrt(num);idx>=1;idx--){
+                int t=idx*idx;
+                if(t<=num) q.push({num-t,ans+1});
+            }
+        }
+        return -1;
     }
 };
