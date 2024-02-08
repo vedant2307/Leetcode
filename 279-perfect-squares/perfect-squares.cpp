63265e6d5248fe325394d26d1e153e0834fbcbb3
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-02-08 15:07:25 +0530

    Time: 2383 ms (5.01%) | Memory: 476.6 MB (5.32%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int numSquares(int n) {
-        queue<pair<int,int>>q;
-        q.push({n,0});
+        queue<int>q;
+        q.push(n);
+        int ans=0;
         while(!q.empty()){
-            auto front=q.front(); q.pop();
-            int num=front.first, ans=front.second;
-            if(num==0) return ans;
-            for(int idx=sqrt(num);idx>=1;idx--){
-                int t=idx*idx;
-                if(t<=num) q.push({num-t,ans+1});
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int num=q.front(); q.pop();
+                if(num==0) return ans;
+                for(int idx=sqrt(num);idx>=1;idx--){
+                    int t=idx*idx;
+                    if(t<=num) q.push(num-t);
+                }
             }
+            ans++;
         }
         return -1;
     }
