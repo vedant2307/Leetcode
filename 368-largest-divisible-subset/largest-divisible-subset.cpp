95f213cb56cfda40bdb65c1addb4a693f7e9bfe5
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-02-10 18:36:34 +0530

    Time: 12 ms (92.51%) | Memory: 11.4 MB (32.74%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        int n = size(nums), max_i = 0;
-		// initially dp[i]=1 since we can always form subset of size=1 ending at i. 
-		// pred[i]=-1 because we havent found any predecessors for any subset yet
-        vector<int> dp(n, 1), pred(n, -1), ans;
-        for(int i = 1; i < n; i++) {          
-            for(int j = 0; j < i; j++)     
-				// nums[i] should divide nums[j] if it is to be included in its subset (i.e dp[j])
-				// only include nums[i] in subset ending at j if resultant subset size (dp[j]+1) is larger than already possible (dp[i])
-                if(nums[i] % nums[j] == 0 && dp[i] < dp[j]+1)
-                    dp[i] = dp[j]+1, pred[i] = j;  // jth element will be predecessor to subset ending at ith element
-            if(dp[i] > dp[max_i]) max_i = i;       // keep track of index where largest subset ends
+    vector<int> largestDivisibleSubset(vector<int>& arr) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        vector<int>dp(n,1);
+        vector<int>vis(n,0);
+        int maxi=0,last=0;
+        for(int idx=1;idx<n;idx++){
+            vis[idx]=idx;
+            for(int prev=0;prev<idx;prev++){
+                if(arr[idx]%arr[prev]==0 && dp[idx]<dp[prev]+1){
+                    dp[idx]=dp[prev]+1;
+                    vis[idx]=prev;
+                }
+            }
+            if(dp[idx]>maxi){
+                maxi=dp[idx]; last=idx;
+            }
         }
-		// start with index where largest subset ended. Reconstruct from that point to the start
-        for(; max_i >= 0; max_i = pred[max_i]) 
-            ans.push_back(nums[max_i]);
+        vector<int>ans;
+        ans.push_back(arr[last]);
+        while(last!=vis[last]){
+            last=vis[last];
+            ans.push_back(arr[last]); 
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
