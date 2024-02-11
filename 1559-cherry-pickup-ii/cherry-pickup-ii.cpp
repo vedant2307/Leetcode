a0ec2d05a326997e9285b303d7083041e3f3bae2
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-02-11 17:38:39 +0530

    Time: 55 ms (76.33%) | Memory: 20 MB (7.10%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int helper(vector<vector<int>>& grid, int row, int col1, int col2, int n, int m,vector<vector<vector<int>>>&dp){
+        if(row==n-1){
+            if(col1==col2) return grid[row][col1];
+            return grid[row][col1]+grid[row][col2];
+        }
+        if(dp[row][col1][col2]!=-1) return dp[row][col1][col2]; 
+        int ans=0;
+        for(int i=-1;i<=1;i++){
+            for(int j=-1;j<=1;j++){
+                int c1=col1+i, c2=col2+j;
+                if(c1>=0 && c2>=0 && c1<m && c2<m){
+                    if(col1==col2) ans=max(ans,helper(grid,row+1,c1,c2,n,m,dp)+grid[row][col1]);
+                    else ans=max(ans,helper(grid,row+1,c1,c2,n,m,dp)+grid[row][col1]+grid[row][col2]);
+                }
+            }
+        } 
+        return dp[row][col1][col2]=ans;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(m+1,-1)));
+        return helper(grid,0,0,m-1,n,m,dp);
+    }
+};
