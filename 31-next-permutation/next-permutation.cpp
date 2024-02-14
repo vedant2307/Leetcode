e037e3301d43f38ccbeb64d2a946cd2dd9c09c1a
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-02-14 18:02:51 +0530

    Time: 9 ms (5.53%) | Memory: 14.5 MB (24.93%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void Reverse(int i,int j,vector<int>&nums){
+        while(i<j) swap(nums[i++],nums[j--]);
+        return ;
+    }
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int i=n-1,j=n-1;
+        while(i>0 && nums[i]<=nums[i-1]) i--;
+        if(i==0) reverse(nums.begin(),nums.end());
+        else{
+            while(j>=i && nums[j]<=nums[i-1]) j--;
+            swap(nums[i-1],nums[j]);
+            Reverse(i,n-1,nums);
+        }
+        return ;
+    }
+};
