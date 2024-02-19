3d313f9e33c3f0900c51d8b828418bc73f63fff8
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-02-19 19:48:48 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (49.03%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0) return false;
-        while(n>0 && n%2==0) n=n/2;
-        return n==0 || n==1;
+        if(n<=0) return false;
+        return (n&(n-1))==0;
     }
 };
