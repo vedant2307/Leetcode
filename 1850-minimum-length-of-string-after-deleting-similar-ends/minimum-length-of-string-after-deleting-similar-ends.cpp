dc08c69dfe66002b16f942b64dd26a5622fa07a9
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-03-05 17:54:25 +0530

    Time: 30 ms (22.76%) | Memory: 13.8 MB (70.34%) - LeetSync

---
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int n=s.length();
+        int i=0,j=n-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                while(i<j && s[i]==s[i+1]) i++;
+                while(i<j && s[j]==s[j-1]) j--;
+                i++; j--;
+            }
+            else return (j-i+1);
+        }
+        return i==j ? 1 : 0;
+    }
+};
