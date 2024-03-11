d3b5bd0b255d9a5987ffb234a8be0f91f413a5f6
Vedant Malpani <109013676+vedant2307@users.noreply.github.com>
2024-03-11 15:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (30.10%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string ans;
+        unordered_map<char,int>mp;
+        for(int i=0;i<order.size();i++) mp[order[i]]=i; 
+        map<int,int>mp1;
+        for(auto it:s){
+            if(mp.find(it)!=mp.end()) mp1[mp[it]]++;
+            else ans+=it;
+        }
+        for(auto it:mp1){
+            char ch=order[it.first]; 
+            int freq=it.second;
+            while(freq--) ans+=ch;
+        }
+        return ans;
+    }
+};
